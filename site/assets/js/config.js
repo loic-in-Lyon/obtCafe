db52b63206ceabb0e84ebd227323dad0e80db488
Loïc POO-SING <130918150+loic-in-Lyon@users.noreply.github.com>
2026-05-10 14:06:14 +0200

    initial structure

---
+++ site/assets/js/config.js
@@ -0,0 +1,3 @@
+const AIRTABLE_API_KEY = "patQiwi7cJVjCaEhZ.33677fc3c49f0fb0d5687df3ef1fa30d9f6192cb0d0c1fed368b5a4cc7dde7af";
+const AIRTABLE_BASE_ID = "appxS8nR9hWUdxiFO";
+const TABLE_NAME = "Events";
